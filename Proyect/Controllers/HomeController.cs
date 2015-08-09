c8e683d52bccfe3e94abd02bd92851432abd958d
NimerA <nimerabraham@gmail.com>
2015-08-08 20:50:55 -0500

    Base
    Primer Login

---
+++ Proyect/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyect.Models;
+
+namespace Proyect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login_Data LD)
+        {
+            return View("Home",LD);
+        }
+    }
+}
---
+++ Proyect/Models/Login_Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyect.Models
+{
+    public class Login_Data
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
